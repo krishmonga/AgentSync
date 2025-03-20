8fad934e2816395de29db51892bd06ebae1d2b79
krishmonga <158186244+krishmonga@users.noreply.github.com>
2025-03-20 13:15:20 +0530

    Removed all files from repository

--- backend/models/data.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const DataSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: String, required: true },
-  assignedAgent: { type: mongoose.Schema.Types.ObjectId, ref: "Agent" },
-});
-
-module.exports = mongoose.model("Data", DataSchema);
--- backend/models/list.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ListSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  assignedAgent: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
-
-module.exports = mongoose.model("List", ListSchema);
